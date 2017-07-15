b6d95468ffa54021f8ac05f1ab22d268812c08f5
Michael Farjon <michaelfarjon@gmail.com>
2017-07-15 21:32:34 +0300

    added inflation added files i thought i commited

--- currencyConvert.py
+++ currencyConvert.py
@@ -3,15 +3,21 @@ import pandas as pd
 
 def convert(currencies, country, val, year):
     if year < 1960:
-        return -999 # TODO how to reutrn error codes?
+        return -999 # TODO how to return error codes?
     if country == 'USA':
         return val
-    return currencies[currencies['Country Name'].str.match(country)][str(year)].iloc[0]
+    return val*currencies[currencies['Country Name'].str.match(country)][str(year)].iloc[0]
 
+def inflate(year):
+    df = pd.read_csv('inflation.csv',delimiter  ='\t')
+    if year < 1960:
+        return -999 # TODO how to return error codes?
+    return df[str(year)].iloc[0]
 
 if __name__ == '__main__':
     df = pd.read_csv('movie_metadata.csv')
     currencies = pd.read_table('currencyTable.txt')
     country = 'Israel' # for testing
-    print('The value is: ' + str(convert(currencies, country, 1, 2016)))
+    year = 2016
+    print('The value is: ' + str(convert(currencies, country, 1*inflate(year), year)))
 

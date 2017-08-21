072a3c74520f1f9ae2ccade742914fc7b5113bc6
miriost <miri.ostrovsky@gmail.com>
2017-08-21 14:33:50 +0300

    working on the notebook 21.8.17

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ df = clean(df)
 df = fixcurrency(df)
 
 # EDA part:
-#eda.facebook_likes_over_the_years(df)
+eda.facebook_likes_over_the_years(df)
 
 # according to the simple year histogram we can note that we mainly deal with new films
 # from movie facebook likes as a correlation to the year we see that obviously since the facebook was first public, the
@@ -18,13 +18,13 @@ df = fixcurrency(df)
 # There are some exceptions of old movies with high likes, usually "classics"
 # TODO arrows with examples of the exceptions
 
-#eda.facebook_likes_to_score(df)
+eda.facebook_likes_to_score(df)
 # we can see that the the really low score movies (below 5) really don't receive many likes, but other than that, there
 # seems to be no clear correlation between a high score and the number of likes.
 
 # amax = np.argmax(df['budget'])
 # print(amax)
 # print(df['movie_title'][amax] + ' ;' + df['country'][amax] + ' ;' + str(df['budget'][amax]))
-eda.budget_to_score(df)
 
+eda.budget_to_score(df)
 
